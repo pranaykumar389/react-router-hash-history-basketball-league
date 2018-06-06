16bd40396d3c81baea56feaa10c458997f0480cb
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2018-06-05 22:59:26 -0400

    added navbar and setup the routes.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import Home    from './Home';
+import Players from './Players';
+import Teams   from './Teams';
+import Navbar  from './Navbar';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        React Router App
-      </div>
+      <Router>
+          <div>
+            <Navbar />
+            
+            <Route path='/' exact component={Home} />
+            <Route path='/players' component={Players} />
+            <Route path='/teams' component={Teams} />
+          </div>
+      </Router>
     );
   }
 }
